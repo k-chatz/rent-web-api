0eb0996bd1c7421870fd152542daef3d585a6b65
LSmyrnaios <lsmyrnaios@gmail.com>
2019-04-19 14:59:02 +0300

    Add security certificate.

--- scripts/generateKeystore.sh
+++ scripts/generateKeystore.sh
@@ -2,3 +2,5 @@
 cd ../
 keytool -genkeypair -alias tomcat -storetype PKCS12 -keyalg RSA -keysize 2048  -keystore keystore.p12 -validity 9999 -dname "EMAILADDRESS=rent@di.uoa.gr, CN=Rent, O=UOA, OU=TL, L=Athens, S=Greece, C=GR" -ext san=dns:localhost
 
+keytool -export -keystore keystore.p12 -alias tomcat -file rentCertificate.crt
+keytool -importcert -file rentCertificate.crt -alias tomcat -keystore %JRE_HOME%\lib\security\cacerts
