e0cdda0a8078c132bef84ffd090300967e973f7b
LSmyrnaios <lsmyrnaios@gmail.com>
2019-04-19 16:59:45 +0300

    Place token-info in its own class in "SignInResponse.class".
    Co-authored-by: LSmyrnaios <lsmyrnaios@gmail.com>
    Co-authored-by: kwstarikanos <kwstarikanos@gmail.com>

--- src/main/java/gr/uoa/di/rent/payload/responses/SignInResponse.java
+++ src/main/java/gr/uoa/di/rent/payload/responses/SignInResponse.java
@@ -2,17 +2,40 @@ package gr.uoa.di.rent.payload.responses;
 
 public class SignInResponse {
 
-    private String accessToken;
-    private String tokenType = "Bearer";
+    public class Token {
+        private String accessToken;
+        private String tokenType;
 
+        public Token(String accessToken, String tokenType) {
+            this.accessToken = accessToken;
+            this.tokenType = tokenType;
+        }
+
+        public String getAccessToken() {
+            return accessToken;
+        }
+
+        public void setAccessToken(String accessToken) {
+            this.accessToken = accessToken;
+        }
+
+        public String getTokenType() {
+            return tokenType;
+        }
+
+        public void setTokenType(String tokenType) {
+            this.tokenType = tokenType;
+        }
+    }
+    private Token token;
     private Long id;
     private String email;
     private String name;
     private String surname;
     private String role;
 
-    public SignInResponse(String accessToken, Long id, String email, String name, String surname, String role) {
-        this.accessToken = accessToken;
+    public SignInResponse(String accessToken, String tokenType, Long id, String email, String name, String surname, String role) {
+        this.token = new Token(accessToken, tokenType);
         this.id = id;
         this.email = email;
         this.name = name;
@@ -20,20 +43,12 @@ public class SignInResponse {
         this.role = role;
     }
 
-    public String getAccessToken() {
-        return accessToken;
+    public Token getToken() {
+        return token;
     }
 
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
-    public String getTokenType() {
-        return tokenType;
-    }
-
-    public void setTokenType(String tokenType) {
-        this.tokenType = tokenType;
+    public void setToken(Token token) {
+        this.token = token;
     }
 
     public Long getId() {
