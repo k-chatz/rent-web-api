5dabb591aa4c4cf4407be1a5b59e5afa0ef37b7c
LSmyrnaios <lsmyrnaios@gmail.com>
2019-04-22 02:43:56 +0300

    - Add endpoint to lock the specified users. Only the admin is authorized. - Set currentSchema in database settings inside "application.properties".
    Co-authored-by: LSmyrnaios <lsmyrnaios@gmail.com>
    Co-authored-by: Ted96 <teoanagnosto@gmail.com>

--- src/main/java/gr/uoa/di/rent/controllers/UsersController.java
+++ src/main/java/gr/uoa/di/rent/controllers/UsersController.java
@@ -1,13 +1,19 @@
 package gr.uoa.di.rent.controllers;
 
+import gr.uoa.di.rent.exceptions.AppException;
 import gr.uoa.di.rent.models.User;
 import gr.uoa.di.rent.repositories.UserRepository;
+import gr.uoa.di.rent.security.CurrentUser;
+import gr.uoa.di.rent.security.UserDetailsImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -27,5 +33,29 @@ public class UsersController {
     @PreAuthorize("hasAuthority('ADMIN')") //this works !!
     public List<User> getUsers() {
         return userRepository.findAll();
+    }
+
+
+    @PostMapping("/lockUsers")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Resource> lockUsers(@RequestParam("userIDs") List<Long> userIDs, @Valid @CurrentUser UserDetailsImpl currentUser) {
+
+        //logger.debug("UserIDs: " + userIDs.toString());
+
+        // Make sure the admin will NOT get locked by mistake!
+        userIDs.remove(currentUser.getId());
+
+        int changed = userRepository.lockUsers(userIDs);
+        if ( changed == 0 ) {
+            logger.error("No users were locked!");
+            throw new AppException("No users were locked!");
+        }
+        else if ( changed != userIDs.size() ) {
+            String errorMsg = "Lock unsuccessful: "  + (userIDs.size() - changed) + " weren't locked.";
+            logger.error(errorMsg);
+            throw new AppException(errorMsg);
+        }
+        else
+            return ResponseEntity.ok().build();
     }
 }
--- src/main/java/gr/uoa/di/rent/repositories/UserRepository.java
+++ src/main/java/gr/uoa/di/rent/repositories/UserRepository.java
@@ -2,8 +2,13 @@ package gr.uoa.di.rent.repositories;
 
 import gr.uoa.di.rent.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -15,4 +20,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     // Returns a user with a specific username
     Optional<User> findByUsername(String username);
+
+    // Lock the users of the given list.
+    @Transactional
+    @Modifying
+    @Query(value="UPDATE users SET locked = true WHERE id IN :userIDs", nativeQuery = true)
+    int lockUsers(@Param("userIDs") List<Long> userIDs);
 }
