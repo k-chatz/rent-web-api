777038c2f3abb8f2a0bcbf425611f3cc0b2aec63
Costas Chatzopoulos <kwstarikanos@gmail.com>
2019-04-21 13:31:30 +0300

    GenerateKeystore.sh: storepass parameter, updated certificate
    Co-authored-by: kwstarikanos <kwstarikanos@gmail.com>

--- scripts/generateKeystore.sh
+++ scripts/generateKeystore.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 cd ../
-keytool -genkeypair -alias tomcat -storetype PKCS12 -keyalg RSA -keysize 2048  -keystore keystore.p12 -validity 9999 -dname "EMAILADDRESS=rent@di.uoa.gr, CN=Rent, O=UOA, OU=TL, L=Athens, S=Greece, C=GR" -ext san=dns:localhost
-
-keytool -export -keystore keystore.p12 -alias tomcat -file rentCertificate.crt
-keytool -importcert -file rentCertificate.crt -alias tomcat -keystore %JRE_HOME%\lib\security\cacerts
+keytool -genkeypair -alias tomcat -storetype PKCS12 -keyalg RSA -keysize 2048 -storepass rent_project -keystore keystore.p12 -validity 9999 -dname "EMAILADDRESS=rent@di.uoa.gr, CN=Rent, O=UOA, OU=TL, L=Athens, S=Greece, C=GR" -ext san=dns:localhost
+keytool -export -keystore keystore.p12 -alias tomcat -file rentCertificate.crt -storepass rent_project
+keytool -importcert -file rentCertificate.crt -alias tomcat -storepass rent_project -keystore %JRE_HOME%\lib\security\cacerts
