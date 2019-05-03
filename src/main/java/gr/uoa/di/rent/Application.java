b532bde7d2107a6444a046422b5f2ef033bc615d
LSmyrnaios <lsmyrnaios@gmail.com>
2019-05-03 23:05:15 +0300

    Add Scheduling functionality.

--- src/main/java/gr/uoa/di/rent/Application.java
+++ src/main/java/gr/uoa/di/rent/Application.java
@@ -14,6 +14,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -27,6 +28,7 @@ import java.util.Date;
 @EnableConfigurationProperties({
         FileStorageProperties.class
 })
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/gr/uoa/di/rent/components/ScheduledTasks.java
@@ -0,0 +1,23 @@
+package gr.uoa.di.rent.components;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+@Component
+public class ScheduledTasks {
+
+    private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 120000)   // Every 2 mins.
+    public void reportCurrentTime() {
+        logger.info("Server is live! Time is now {}", dateFormat.format(new Date()));
+    }
+}
