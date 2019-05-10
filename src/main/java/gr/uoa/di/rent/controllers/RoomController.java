893a7f1d642830f63be53bb5ebf3eafbf15111f9
LSmyrnaios <lsmyrnaios@gmail.com>
2019-05-10 15:50:57 +0300

    Change Room-endpoints base-url-path. (now every room-endpoint its under its hotel)

--- src/main/java/gr/uoa/di/rent/controllers/RoomController.java
+++ src/main/java/gr/uoa/di/rent/controllers/RoomController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Validated
-@RequestMapping("/rooms")
+@RequestMapping("hotels/{hotelId}/rooms")
 public class RoomController {
 
     private static final Logger logger = LoggerFactory.getLogger(RoomController.class);
