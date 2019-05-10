ee3908e9f622dd7fca8493c2ca13d6f3bf1d0692
LSmyrnaios <lsmyrnaios@gmail.com>
2019-05-10 22:31:53 +0300

    Add method-declarations in "FileRepository", to be used later by the endpoints.

--- src/main/java/gr/uoa/di/rent/repositories/FileRepository.java
+++ src/main/java/gr/uoa/di/rent/repositories/FileRepository.java
@@ -1,6 +1,9 @@
 package gr.uoa.di.rent.repositories;
 
 import gr.uoa.di.rent.models.File;
+import gr.uoa.di.rent.models.Hotel;
+import gr.uoa.di.rent.models.Room;
+import gr.uoa.di.rent.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +13,19 @@ import java.util.List;
 public interface FileRepository extends JpaRepository<File, Long> {
 
     List<File> findAll();
-    File findById(long id);
+
+
+    List<File> findAllByUploader(User user);
+
+    void deleteAllByUploader(User user);
+
+
+    List<File> findAllByHotel(Hotel hotel);
+
+    void deleteAllByHotel(Hotel hotel);
+
+
+    List<File> findAllByRoom(Room room);
+
+    void deleteAllByRoom(Room room);
 }
