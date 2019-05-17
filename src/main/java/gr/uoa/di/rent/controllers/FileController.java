cf65fbad2fca34bd382900b41becc62ae67bc66d
peterthunder <morfiristhunder@hotmail.gr>
2019-05-17 23:54:05 +0300

    Change CONTENT_DISPOSITION header from attachment to inline. - Change the header from attachment to inline in GetFileResponse function in file controller so that when you enter the link in the browser the image does not get downloaded but displayed.
    Co-authored-by: kwstarikanos <kwstarikanos@gmail.com>
    Co-authored-by: antoni4040 <ant.karvelas@gmail.com>
    Co-authored-by: Ted96 <teoanagnosto@gmail.com>
    Co-authored-by: LSmyrnaios <lsmyrnaios@gmail.com>
    Co-authored-by: petethunder <morfiristhunder@hotmail.gr>

--- src/main/java/gr/uoa/di/rent/controllers/FileController.java
+++ src/main/java/gr/uoa/di/rent/controllers/FileController.java
@@ -116,7 +116,7 @@ public class FileController {
 
         return ResponseEntity.ok()
                 .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + resource.getFilename() + "\"")
                 .body(resource);
     }
 
