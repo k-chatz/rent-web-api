2036df18e61c0671a003e6fc6bf7ced918321e8f
peterthunder <morfiristhunder@hotmail.gr>
2019-05-18 00:36:32 +0300

    Create room controller, update hotel and room controllers. - Move the commented code from the hotel controller to the re-added room controller. - Code cleanup.
    Co-authored-by: Ted96 <teoanagnosto@gmail.com>
    Co-authored-by: petethunder <morfiristhunder@hotmail.gr>

--- src/main/java/gr/uoa/di/rent/controllers/HotelController.java
+++ src/main/java/gr/uoa/di/rent/controllers/HotelController.java
@@ -1,13 +1,8 @@
 package gr.uoa.di.rent.controllers;
 
-import gr.uoa.di.rent.exceptions.ApiError;
 import gr.uoa.di.rent.models.*;
 import gr.uoa.di.rent.payload.requests.HotelRequest;
-import gr.uoa.di.rent.payload.requests.ReservationRequest;
-import gr.uoa.di.rent.payload.requests.filters.PagedResponseFilter;
 import gr.uoa.di.rent.payload.responses.HotelResponse;
-import gr.uoa.di.rent.payload.responses.PagedResponse;
-import gr.uoa.di.rent.payload.responses.RoomResponse;
 import gr.uoa.di.rent.repositories.*;
 import gr.uoa.di.rent.security.CurrentUser;
 import gr.uoa.di.rent.security.Principal;
@@ -21,14 +16,10 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
-
-import static java.time.temporal.ChronoUnit.DAYS;
 
 @RestController
 @Validated
@@ -41,31 +32,11 @@ public class HotelController {
 
     private final BusinessRepository businessRepository;
 
-    private final RoomRepository roomRepository;
-
-    private final HotelRepository hotelRepository;
-
-    private final CalendarRepository calendarRepository;
-
-    private final UserRepository userRepository;
-
-    private final TransactionRepository transactionRepository;
-
-    private final ReservationRepository reservationRepository;
-
     private final AtomicInteger counter = new AtomicInteger();
 
-    public HotelController(HotelService hotelService, BusinessRepository businessRepository, RoomRepository roomRepository, HotelRepository hotelRepository,
-                           UserRepository userRepository, CalendarRepository calendarRepository, TransactionRepository transactionRepository,
-                           ReservationRepository reservationRepository) {
+    public HotelController(HotelService hotelService, BusinessRepository businessRepository) {
         this.hotelService = hotelService;
         this.businessRepository = businessRepository;
-        this.roomRepository = roomRepository;
-        this.hotelRepository = hotelRepository;
-        this.userRepository = userRepository;
-        this.calendarRepository = calendarRepository;
-        this.transactionRepository = transactionRepository;
-        this.reservationRepository = reservationRepository;
     }
 
 
@@ -109,191 +80,4 @@ public class HotelController {
         return hotelService.findHotelByID(hotelId);
     }
 
-
-    @GetMapping("/{hotelId}/rooms")
-    public List<Room> getHotelRooms(@PathVariable(value = "hotelId") Long hotelId) {
-
-        return roomRepository.findAllByHotel_id(hotelId);
-    }
-
-    @GetMapping("/{hotelId}/rooms/paged")
-    public PagedResponse<Room> getHotelRoomsPaginated(PagedResponseFilter pagedResponseFilter) {
-
-        return null;
-    }
-
-    @GetMapping("/{hotelId}/rooms/{roomId:[\\d]+}")
-    public ResponseEntity<?> getHotelRoom(@PathVariable(value = "hotelId") Long hotelId, @PathVariable(value = "roomId") Long roomId) {
-
-        // Check if the given hotel exists.
-        Optional<Hotel> hotel = hotelRepository.findById(hotelId);
-        if (!hotel.isPresent())
-            return ResponseEntity.badRequest().body("No hotel exists with id = " + hotelId);
-
-        // Check and return the room.
-        Optional<Room> room = roomRepository.findById(roomId);
-        if (room.isPresent())
-            return ResponseEntity.ok(new RoomResponse(room.get()));
-        else
-            return ResponseEntity.badRequest().body("No room with id = " + roomId + " was found in hotel with id = " + hotelId);
-    }
-
-    @PostMapping("/{hotelId}/rooms/{roomId:[\\d]+}/reservation")
-    public ResponseEntity<?> reservation(
-            @CurrentUser Principal principal,
-            @PathVariable(value = "hotelId") Long hotelId,
-            @PathVariable(value = "roomId") Long roomId,
-            @Valid @RequestBody ReservationRequest reservationRequest
-    ) {
-
-        // Check if hotel exists
-        Optional<Hotel> hotel = hotelRepository.findById(hotelId);
-        if (!hotel.isPresent())
-            return ResponseEntity.badRequest().body(
-                    new ApiError(
-                            HttpStatus.BAD_REQUEST,
-                            "Hotel does not exist!",
-                            Collections.singletonList("No hotel was found with id " + hotelId)));
-
-        // Check if room exists.
-        Optional<Room> room = roomRepository.findById(roomId);
-        if (!room.isPresent())
-            return ResponseEntity.badRequest().body(
-                    new ApiError(
-                            HttpStatus.BAD_REQUEST,
-                            "The requested room was not found in the requested hotel!",
-                            Collections.singletonList("Room with id " + roomId + " was not found in hotel with id " + hotelId)));
-
-        // Check if valid date format given
-        if (reservationRequest.getEndDate().isBefore(reservationRequest.getStartDate()))
-            return ResponseEntity.badRequest().body(
-                    new ApiError(
-                            HttpStatus.BAD_REQUEST,
-                            "Check-out day cannot be before Check-in day!",
-                            Collections.singletonList("Check-out day " + reservationRequest.getEndDate() +
-                                    " cannot be before Check-in day " + reservationRequest.getStartDate())));
-
-        // Check if reservation is for 0 days   ( ex:  from  30/6/2019 to  30/6/2019 )
-        if (reservationRequest.getEndDate().isEqual(reservationRequest.getStartDate()))
-            return ResponseEntity.badRequest().body(
-                    new ApiError(
-                            HttpStatus.BAD_REQUEST,
-                            "Cannot reserve a room for 0 Days!",
-                            Collections.singletonList("Check-out date " + reservationRequest.getEndDate() +
-                                    " is the same as Check-in date " + reservationRequest.getStartDate())));
-
-        // Check calendar (if already reserved one of these days)
-        if (!isAvailable(reservationRequest.getStartDate(), reservationRequest.getEndDate(), roomId))
-            return ResponseEntity.badRequest().body(
-                    new ApiError(
-                            HttpStatus.BAD_REQUEST,
-                            "The requested room is not available these days!",
-                            Collections.singletonList("Room " + roomId + " of hotel " + hotelId +
-                                    " is not available all the days between " + reservationRequest.getStartDate() + " and " +
-                                    reservationRequest.getEndDate())));
-
-
-        int total_price = (int) (room.get().getPrice() * DAYS.between(reservationRequest.getStartDate(), reservationRequest.getEndDate()));
-
-        Transaction transaction = new Transaction(principal.getUser(), hotel.get().getBusiness(), total_price);
-        
-        Calendar calendar = new Calendar(
-                reservationRequest.getStartDate(),
-                reservationRequest.getEndDate(),
-                null, room.get()
-        );
-        Reservation reservation = new Reservation(room.get(), null, calendar);
-
-        calendar.setReservation(reservation);
-
-        Transaction transaction_s = transactionRepository.save(transaction);
-        reservation.setTransaction(transaction_s);
-
-        calendarRepository.save(calendar);
-
-        //execute sql query to subtract money from user, and add said money to business' wallet + admin's business wallet
-        hotelRepository.transferMoney(principal.getUser().getId(), hotelId, (double) total_price);
-
-        return ResponseEntity.ok().body("Room Successfully Booked!");
-    }
-
-    private boolean isAvailable(LocalDate startDate, LocalDate endDate, Long roomID) {
-        List<Calendar> calendars = calendarRepository.isRoomAvailable(startDate, endDate, roomID);
-        return calendars.isEmpty();
-    }
-
-/*    @RequestMapping("/{hotelId}/rooms")
-    ModelAndView redirectToRooms(@PathVariable(value = "hotelId") Long hotelId){
-        return new ModelAndView("redirect:/rooms", hotelId);
-    }*/
-
 }
-
-
-//@RestController
-//@Validated
-//@RequestMapping("hotels/{hotelId}/rooms")
-//public class RoomController {
-//
-//    private static final Logger logger = LoggerFactory.getLogger(RoomController.class);
-//
-//    @Autowired
-//    private RoomRepository roomRepository;
-//
-//    @Autowired
-//    private HotelRepository hotelRepository;
-//
-//    private final AtomicInteger counter = new AtomicInteger();
-//
-//    @GetMapping("")
-//    public List<Room> getHotelRooms(@PathVariable(value = "hotelId") Long hotelId) {
-//
-//        return roomRepository.findAllByHotel_id(hotelId);
-//    }
-//
-//    @GetMapping("paged")
-//    public PagedResponse<Room> getHotelRoomsPaginated(PagedResponseFilter pagedResponseFilter) {
-//
-//        return null;
-//    }
-//
-//    @GetMapping("{roomId:[\\d]+}")
-//    public ResponseEntity<?> getHotelRoom(@PathVariable(value = "hotelId") Long hotelId, @PathVariable(value = "roomId") Long roomId) {
-//
-//        // Check if the given hotel exists.
-//        Optional<Hotel> hotel = hotelRepository.findById(hotelId);
-//        if (!hotel.isPresent())
-//            return ResponseEntity.badRequest().body("No hotel exists with id = " + hotelId);
-//
-//        // Check and return the room.
-//        Optional<Room> room = roomRepository.findById(roomId);
-//        if (room.isPresent())
-//            return ResponseEntity.ok(new RoomResponse(room.get()));
-//        else
-//            return ResponseEntity.badRequest().body("No room with id = " + roomId + " was found in hotel with id = " + hotelId);
-//    }
-//
-//
-//    @GetMapping("{roomId:[\\d]+}/reservation")
-//    public ResponseEntity<?> reservation(
-//            @CurrentUser Principal principal,
-//            @PathVariable(value = "hotelId") Long hotelId,
-//            @PathVariable(value = "roomId") Long roomId
-//    ) {
-//        // Check if the given hotel exists.
-//        Optional<Hotel> hotel = hotelRepository.findById(hotelId);
-//        if (!hotel.isPresent())
-//            return ResponseEntity.badRequest().body("No hotel exists with id = " + hotelId);
-//
-//        // Check and return the room.
-//        Optional<Room> room = roomRepository.findById(roomId);
-//        if (room.isPresent()) {
-//
-//        }
-//
-//        Double balance = principal.getUser().getWallet().getBalance();
-//
-//
-//        return ResponseEntity.ok(principal);
-//    }
-//}
---
+++ src/main/java/gr/uoa/di/rent/controllers/RoomController.java
@@ -0,0 +1,165 @@
+package gr.uoa.di.rent.controllers;
+
+import gr.uoa.di.rent.exceptions.ApiError;
+import gr.uoa.di.rent.models.*;
+import gr.uoa.di.rent.payload.requests.ReservationRequest;
+import gr.uoa.di.rent.payload.requests.filters.PagedResponseFilter;
+import gr.uoa.di.rent.payload.responses.PagedResponse;
+import gr.uoa.di.rent.payload.responses.RoomResponse;
+import gr.uoa.di.rent.repositories.*;
+import gr.uoa.di.rent.security.CurrentUser;
+import gr.uoa.di.rent.security.Principal;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+
+@RestController
+@Validated
+@RequestMapping("/hotels/{hotelId}/rooms")
+public class RoomController {
+
+    private static final Logger logger = LoggerFactory.getLogger(RoomController.class);
+
+    private final RoomRepository roomRepository;
+
+    private final HotelRepository hotelRepository;
+
+    private final CalendarRepository calendarRepository;
+
+    private final TransactionRepository transactionRepository;
+
+    private final AtomicInteger counter = new AtomicInteger();
+
+    public RoomController(RoomRepository roomRepository, HotelRepository hotelRepository,
+                          CalendarRepository calendarRepository, TransactionRepository transactionRepository) {
+        this.roomRepository = roomRepository;
+        this.hotelRepository = hotelRepository;
+        this.calendarRepository = calendarRepository;
+        this.transactionRepository = transactionRepository;
+
+    }
+
+    @GetMapping("")
+    public List<Room> getHotelRooms(@PathVariable(value = "hotelId") Long hotelId) {
+
+        return roomRepository.findAllByHotel_id(hotelId);
+    }
+
+    @GetMapping("/paged")
+    public PagedResponse<Room> getHotelRoomsPaginated(PagedResponseFilter pagedResponseFilter) {
+
+        return null;
+    }
+
+    @GetMapping("/{roomId:[\\d]+}")
+    public ResponseEntity<?> getHotelRoom(@PathVariable(value = "hotelId") Long hotelId, @PathVariable(value = "roomId") Long roomId) {
+
+        // Check if the given hotel exists.
+        Optional<Hotel> hotel = hotelRepository.findById(hotelId);
+        if (!hotel.isPresent())
+            return ResponseEntity.badRequest().body("No hotel exists with id = " + hotelId);
+
+        // Check and return the room.
+        Optional<Room> room = roomRepository.findById(roomId);
+        if (room.isPresent())
+            return ResponseEntity.ok(new RoomResponse(room.get()));
+        else
+            return ResponseEntity.badRequest().body("No room with id = " + roomId + " was found in hotel with id = " + hotelId);
+    }
+
+   @PostMapping("/{roomId:[\\d]+}/reservation")
+   @PreAuthorize("hasRole('USER') or hasRole('PROVIDER')")
+   public ResponseEntity<?> reservation(
+           @CurrentUser Principal principal, @PathVariable(value = "hotelId") Long hotelId,
+           @PathVariable(value = "roomId") Long roomId, @Valid @RequestBody ReservationRequest reservationRequest
+   ) {
+
+       // Check if hotel exists
+       Optional<Hotel> hotel = hotelRepository.findById(hotelId);
+       if (!hotel.isPresent())
+           return ResponseEntity.badRequest().body(
+                   new ApiError(
+                           HttpStatus.BAD_REQUEST,
+                           "Hotel does not exist!",
+                           Collections.singletonList("No hotel was found with id " + hotelId)));
+
+       // Check if room exists.
+       Optional<Room> room = roomRepository.findById(roomId);
+       if (!room.isPresent())
+           return ResponseEntity.badRequest().body(
+                   new ApiError(
+                           HttpStatus.BAD_REQUEST,
+                           "The requested room was not found in the requested hotel!",
+                           Collections.singletonList("Room with id " + roomId + " was not found in hotel with id " + hotelId)));
+
+       // Check if valid date format given
+       if (reservationRequest.getEndDate().isBefore(reservationRequest.getStartDate()))
+           return ResponseEntity.badRequest().body(
+                   new ApiError(
+                           HttpStatus.BAD_REQUEST,
+                           "Check-out day cannot be before Check-in day!",
+                           Collections.singletonList("Check-out day " + reservationRequest.getEndDate() +
+                                   " cannot be before Check-in day " + reservationRequest.getStartDate())));
+
+       // Check if reservation is for 0 days   ( ex:  from  30/6/2019 to  30/6/2019 )
+       if (reservationRequest.getEndDate().isEqual(reservationRequest.getStartDate()))
+           return ResponseEntity.badRequest().body(
+                   new ApiError(
+                           HttpStatus.BAD_REQUEST,
+                           "Cannot reserve a room for 0 Days!",
+                           Collections.singletonList("Check-out date " + reservationRequest.getEndDate() +
+                                   " is the same as Check-in date " + reservationRequest.getStartDate())));
+
+       // Check calendar (if already reserved one of these days)
+       if (!isAvailable(reservationRequest.getStartDate(), reservationRequest.getEndDate(), roomId))
+           return ResponseEntity.badRequest().body(
+                   new ApiError(
+                           HttpStatus.BAD_REQUEST,
+                           "The requested room is not available these days!",
+                           Collections.singletonList("Room " + roomId + " of hotel " + hotelId +
+                                   " is not available all the days between " + reservationRequest.getStartDate() + " and " +
+                                   reservationRequest.getEndDate())));
+
+
+       int total_price = (int) (room.get().getPrice() * DAYS.between(reservationRequest.getStartDate(), reservationRequest.getEndDate()));
+
+       Transaction transaction = new Transaction(principal.getUser(), hotel.get().getBusiness(), total_price);
+
+       Calendar calendar = new Calendar(
+               reservationRequest.getStartDate(),
+               reservationRequest.getEndDate(),
+               null, room.get()
+       );
+       Reservation reservation = new Reservation(room.get(), null, calendar);
+
+       calendar.setReservation(reservation);
+
+       Transaction transaction_s = transactionRepository.save(transaction);
+       reservation.setTransaction(transaction_s);
+
+       calendarRepository.save(calendar);
+
+       //execute sql query to subtract money from user, and add said money to business' wallet + admin's business wallet
+       hotelRepository.transferMoney(principal.getUser().getId(), hotelId, (double) total_price);
+
+       return ResponseEntity.ok().body("Room Successfully Booked!");
+   }
+
+    private boolean isAvailable(LocalDate startDate, LocalDate endDate, Long roomID) {
+        List<Calendar> calendars = calendarRepository.getOverlappingCalendars(startDate, endDate, roomID);
+        return calendars.isEmpty();
+    }
+}
