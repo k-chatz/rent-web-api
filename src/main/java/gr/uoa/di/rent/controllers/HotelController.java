aa4fb266d0e559dc05576ae48d8ab219e814caaa
peterthunder <morfiristhunder@hotmail.gr>
2019-05-25 05:26:49 +0300

    Create new sql queries for hotel search and update hotelController. - Update the new insertHotel endpoint to accept amenities and only insert those that exist in the database before inserting the hotel. - Update the search endpoint to create a list of the amenities(filters) that were given as the query params and then query the database to find hotels based on start_date, end_date, lat, long, a certain radius in kilometers and the amenities(filters). - Refactor code.
    .
    
    Co-authored-by: petethunder <morfiristhunder@hotmail.gr>
    Co-authored-by: Ted96 <teoanagnosto@gmail.com>

--- src/main/java/gr/uoa/di/rent/controllers/HotelController.java
+++ src/main/java/gr/uoa/di/rent/controllers/HotelController.java
@@ -9,16 +9,12 @@ import gr.uoa.di.rent.payload.responses.PagedResponse;
 import gr.uoa.di.rent.repositories.*;
 import gr.uoa.di.rent.security.CurrentUser;
 import gr.uoa.di.rent.security.Principal;
-import gr.uoa.di.rent.services.HotelService;
 import gr.uoa.di.rent.util.AppConstants;
 import gr.uoa.di.rent.util.ModelMapper;
 import gr.uoa.di.rent.util.PaginatedResponseUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -26,11 +22,10 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.lang.reflect.Field;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @RestController
 @Validated
@@ -39,20 +34,21 @@ public class HotelController {
 
     private static final Logger logger = LoggerFactory.getLogger(HotelController.class);
 
-    private final HotelService hotelService;
-
     private final BusinessRepository businessRepository;
 
     private final HotelRepository hotelRepository;
 
+    private final AmenitiesRepository amenitiesRepository;
+
     private final AtomicInteger counter = new AtomicInteger();
 
-    public HotelController(HotelService hotelService,
-                           BusinessRepository businessRepository,
-                           HotelRepository hotelRepository) {
-        this.hotelService = hotelService;
+    public HotelController(BusinessRepository businessRepository,
+                           HotelRepository hotelRepository,
+                           AmenitiesRepository amenitiesRepository) {
         this.businessRepository = businessRepository;
         this.hotelRepository = hotelRepository;
+        this.amenitiesRepository = amenitiesRepository;
+
     }
 
 
@@ -62,7 +58,7 @@ public class HotelController {
 
         User provider = principal.getUser();
 
-        // Get business for this provider (principal).
+        /* Get business for this provider (principal). */
         Optional<Business> business_opt = businessRepository.findById(provider.getBusiness().getId());
         if (!business_opt.isPresent()) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("No business was found for provider with username: " + provider.getUsername());
@@ -72,46 +68,47 @@ public class HotelController {
 
         Business business = business_opt.get();
 
-        // Create a hotel object which will be under that business.
+        /* Create a hotel object which will belong to that business. */
         Hotel hotel = hotelRequest.asHotel(business.getId());
-
         hotel.setBusiness(business);
 
-        List<Room> rooms = new ArrayList<Room>();
-
+        List<Room> rooms = new ArrayList<>();
         hotel.setRooms(rooms);
 
         List<File> hotel_photos = new ArrayList<>();
         hotel.setHotel_photos(hotel_photos);
 
-        // Store the hotel in the database.
-        hotel = hotelService.createHotel(hotel);
-        // Get the new hotel-object having its id assigned by the database.
+        /* Get list of amenities from request */
+        Collection<Amenity> amenities = amenitiesRepository.findAll()
+                .stream()
+                .filter(amenity -> hotelRequest.getAmenities().contains(amenity.getName()))
+                .collect(Collectors.toList());
+
+        hotel.setAmenities(amenities);
+
+        /* Store the hotel in the database. */
+        hotel = hotelRepository.save(hotel);
+
         return ResponseEntity.ok(new HotelResponse(hotel));
     }
 
     @GetMapping("/{hotelId:[\\d]+}")
     public ResponseEntity<?> getHotelByID(@PathVariable(value = "hotelId") Long hotelId) {
 
-        Hotel hotel = hotelService.findHotelByID(hotelId);
-        if ( hotel == null ) {
+        Optional<Hotel> hotel = hotelRepository.findById(hotelId);
+        if (!hotel.isPresent()) {
             logger.warn("No hotel exists with id = " + hotelId);
             return ResponseEntity.notFound().build();
-        }
-        else
-            return ResponseEntity.ok(new HotelResponse(hotel));
+        } else
+            return ResponseEntity.ok(new HotelResponse(hotel.get()));
     }
 
     @GetMapping("/search")
-    public PagedResponse<Hotel> searchHotels(@Valid PagedHotelsFilter pagedHotelsFilters){
-
-        System.out.println("Sort-field is: "+ pagedHotelsFilters.getSort_field());
-
-
+    public PagedResponse<Hotel> searchHotels(@Valid PagedHotelsFilter pagedHotelsFilters) {
 
         try {
-            PaginatedResponseUtil.validateParameters( pagedHotelsFilters.getPage(),
-                    pagedHotelsFilters.getSize(), pagedHotelsFilters.getSort_field(), Hotel.class);
+            PaginatedResponseUtil.validateParameters(pagedHotelsFilters.getPage(), pagedHotelsFilters.getSize(),
+                    pagedHotelsFilters.getSort_field(), Hotel.class);
         } catch (BadRequestException bre) {
             throw bre;
         } catch (Exception e) {
@@ -120,18 +117,50 @@ public class HotelController {
 
         Sort.Direction sort_order;
 
-        // Default order is ASC, otherwise DESC
+        /* Default order is ASC, otherwise DESC */
         if (AppConstants.DEFAULT_ORDER.equals(pagedHotelsFilters.getOrder()))
             sort_order = Sort.Direction.ASC;
         else
             sort_order = Sort.Direction.DESC;
 
+        /* Create a list with all the amenity filters that are to be applied */
+        Field[] fields = pagedHotelsFilters.getClass().getDeclaredFields();
+
+        List<String> queryAmenities = new ArrayList<>();
+        Arrays.stream(fields)
+                .filter(field -> {
+                            field.setAccessible(true);
+                            try {
+                                return field.get(pagedHotelsFilters).equals(true) && AppConstants.amenity_names.contains(field.getName());
+                            } catch (IllegalAccessException e) {
+                                e.printStackTrace();
+                                throw new BadRequestException("An error occurred accessing a field when trying to create the amenity filter list!");
+                            }
+                        }
+                )
+                .map(Field::getName)
+                .forEach(queryAmenities::add);
+
         Pageable pageable = PageRequest.of(pagedHotelsFilters.getPage(), pagedHotelsFilters.getSize(),
                 sort_order, pagedHotelsFilters.getSort_field());
 
+        double radius_km = 1000000000000000.0;
 
-        // TODO: Na allax8oyn ola ta pedia sta models se camel case giati alliws vgazei error -> reproduce with sort_field=description_short
-        Page<Hotel> hotels =  hotelRepository.findAll(pageable);
+        /* Get All Hotels  */
+        Page<Hotel> hotels;
+
+        /* If no amenities were given, search only with the basic filters */
+        //TODO Add price range and rating filters to the sql query.
+        if (!queryAmenities.isEmpty()) {
+            hotels = hotelRepository.findWithFilters(
+                    pagedHotelsFilters.getStart_date(), pagedHotelsFilters.getEnd_date(),
+                    pagedHotelsFilters.getLng(), pagedHotelsFilters.getLat(), radius_km,
+                    pagedHotelsFilters.getVisitors(),
+                    queryAmenities, queryAmenities.size(),
+                    pageable);
+        } else {
+            hotels = hotelRepository.findAll(pageable);
+        }
 
         if (hotels.getNumberOfElements() == 0) {
             return new PagedResponse<>(Collections.emptyList(), hotels.getNumber(),
--- src/main/java/gr/uoa/di/rent/repositories/HotelRepository.java
+++ src/main/java/gr/uoa/di/rent/repositories/HotelRepository.java
@@ -1,6 +1,8 @@
 package gr.uoa.di.rent.repositories;
 
 import gr.uoa.di.rent.models.Hotel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -8,6 +10,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -41,9 +44,110 @@ public interface HotelRepository extends JpaRepository<Hotel, Long> {
             "  and b.wallet = wallets.id;\n" +
             "\n" +
             "COMMIT;", nativeQuery = true)
-    int transferMoney(@Param("userID") Long userID, @Param("hotelID") Long hotelID, @Param("amount") Double amount);
-}
+    void transferMoney(@Param("userID") Long userID, @Param("hotelID") Long hotelID, @Param("amount") Double amount);
 
+
+    @Query(value = "SELECT h.*\n" +
+            "FROM hotels as h,\n" +
+            "       hotel_amenities as ha,\n" +
+            "       amenities as a\n" +
+            "       WHERE ha.amenity_id = a.id\n" +
+            "       and ha.hotel_id = h.id\n" +
+            "       AND a.name in :amenities\n" +
+            "       GROUP BY h.id\n" +
+            "       HAVING COUNT(DISTINCT a.id) = :amenities_count",
+            countQuery = "SELECT COUNT(h.*)\n" +
+                    "FROM hotels as h,\n" +
+                    "       hotel_amenities as ha,\n" +
+                    "       amenities as a\n" +
+                    "       WHERE ha.amenity_id = a.id\n" +
+                    "       AND ha.hotel_id = h.id\n" +
+                    "       AND a.name in :amenities\n" +
+                    "       GROUP BY h.id\n" +
+                    "       HAVING COUNT(DISTINCT a.id) = :amenities_count",
+            nativeQuery = true)
+    Page<Hotel> findAllHotelsByAmenities(@Param("amenities") List<String> amenities,
+                                         @Param("amenities_count") int amenities_count,
+                                         Pageable pageable);
+
+    @Query(value =
+            "SELECT h.*\n" +
+            "FROM hotels h\n" +
+            "WHERE (point(:longitude, :latitude) <@> point(lng, lat)) < :radius_km / 1.61",
+            countQuery = "SELECT COUNT(h.*)\n" +
+                    "FROM hotels h\n" +
+                    "WHERE (point(:longitude, :latitude) <@> point(lng, lat)) < :radius_km / 1.61",
+            nativeQuery = true)
+    Page<Hotel> findByLocationAndRadius(@Param("longitude") double longitude, @Param("latitude") double latitude, @Param("radius_km") double radius_km, Pageable pageable);
+
+    @Query(value =
+            "SELECT h.*\n" +
+            "FROM hotels h,\n" +
+            "    hotel_amenities ha,\n" +
+            "    amenities a\n" +
+            "WHERE\n" +
+            "        h.id in\n" +
+            "        (\n" +
+            "            SELECT hotelsfilter.id\n" +
+            "            FROM hotels hotelsfilter\n" +
+            "            WHERE\n" +
+            "                    hotelsfilter.id in\n" +
+            "                    -- 1) GEOLOCATION RADIUS SEARCH\n" +
+            "                    (\n" +
+            "                        SELECT h1.id\n" +
+            "                        FROM hotels h1\n" +
+            "                        WHERE (point(:longitude, :latitude) <@> point(lng, lat)) < :radius_km / 1.61\n" +
+            "                    )\n" +
+            "              -- 2) VISITORS + CALENDAR AVAILABILITY\n" +
+            "                    and\n" +
+            "                    hotelsfilter.id in (\n" +
+            "                            SELECT DISTINCT h2.id\n" +
+            "                            FROM hotels h2 , rooms r1\n" +
+            "\n" +
+            "                            WHERE\n" +
+            "                                    r1.hotel = h2.id\n" +
+            "                              and\n" +
+            "                                    r1.id in (\n" +
+            "                                    SELECT r.id\n" +
+            "                                    FROM rooms r\n" +
+            "                                    WHERE\n" +
+            "                                            r.capacity >= :visitors\n" +
+            "                                      AND\n" +
+            "                                            r.id NOT IN (\n" +
+            "                                            SELECT r.id\n" +
+            "                                            FROM calendars c\n" +
+            "                                            WHERE\n" +
+            "                                                    c.room = r.id\n" +
+            "                                              AND\n" +
+            "                                                (((c.start_date <= :startDate AND :endDate <= c.end_date)\n" +
+            "                                                    OR (c.start_date <= :endDate AND :endDate <= c.end_date)\n" +
+            "                                                    OR (:startDate < end_date AND :endDate >= c.end_date)))\n" +
+            "\n" +
+            "                                            LIMIT 1  ---maybe not necessary\n" +
+            "                                        )\n" +
+            "                                )\n" +
+            "                    )\n" +
+            "\n" +
+            "        )\n" +
+            "\n" +
+            "  -- 3) AMENITY SEARCH\n" +
+            "  AND ha.amenity_id = a.id\n" +
+            "  AND ha.hotel_id = h.id\n" +
+            "  AND a.name in :amenities\n" +
+            "\n" +
+            "\n" +
+            "GROUP BY h.id\n" +
+            "HAVING COUNT(DISTINCT a.id) = :amenities_count"
+            , nativeQuery = true)
+    Page<Hotel> findWithFilters(
+            @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate,
+            @Param("longitude") double longitude, @Param("latitude") double latitude, @Param("radius_km") double radius_km,
+            @Param("visitors") int visitors,
+            @Param("amenities") List<String> amenities, @Param("amenities_count") int amenities_count,
+            Pageable pageable
+    );
+
+}
 
 //    //Update pending provider to true.
 //    @Transactional
