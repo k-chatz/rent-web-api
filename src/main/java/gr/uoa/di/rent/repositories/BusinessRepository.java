a92f69685089ee52e0f62b53bcf5a53810fef9cc
Costas Chatzopoulos <kwstarikanos@gmail.com>
2019-06-19 00:44:23 +0300

    find all by provider id for business repo

--- src/main/java/gr/uoa/di/rent/repositories/BusinessRepository.java
+++ src/main/java/gr/uoa/di/rent/repositories/BusinessRepository.java
@@ -1,6 +1,8 @@
 package gr.uoa.di.rent.repositories;
 
 import gr.uoa.di.rent.models.Business;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +15,8 @@ public interface BusinessRepository extends JpaRepository<Business, Long> {
     Optional<Business> findById(Long business_id);
 
     List<Business> findAll();
+
+    List<Business> findAllByProviderId(Long id);
+
+    Page<Business> findAllByProviderId(Pageable pageable, Long id);
 }
